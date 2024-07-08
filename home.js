c7a1d09815c51ad46dbfd8576448b18efa29cf13
Alchemist <tnbipin@gmail.com>
2024-07-08 23:49:35 +0530

    6

--- home.js
+++ home.js
@@ -1,6 +1,27 @@
 // document.addEventListener("DOMContentLoaded", (event) => {});
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable)
 
+//overall scroll snap
+// gsap.to(window, {
+//     scrollTrigger: {
+//         snap: .5
+//     }
+// })
+
+//nav-link
+document.querySelectorAll(".nav-link").forEach(
+    link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = this.getAttribute("href");
+            gsap.to(window, {
+                duration: 1,
+                scrollTo: target
+            })
+        })
+    }
+)
+
 // book-now
 Draggable.create(".book-now", {
     bounds: "body",
@@ -18,7 +39,7 @@ gsap.from(".arrow-down", {
     duration: 3
 })
 
-// horizontal scroll
+// info-holder horizontal scroll
 let pinBoxes = document.querySelectorAll(".info-container > *");
 let pinWrap = document.querySelector(".info-container");
 let pinWrapWidth = pinWrap.offsetWidth;
@@ -31,25 +52,16 @@ gsap.to(".info-container", {
         anticipatePin: 1,
         start: "top top",
         end: pinWrapWidth,
-        snap: 0.5
+        snap: 0.5,
+        onEnter: () => {
+            gsap.to(".intro-section", { backgroundColor: "black" }),
+                gsap.to("nav", { backgroundColor: "white" })
+        },
+        onLeaveBack: () => {
+            gsap.to(".intro-section", { backgroundColor: "#d3f0e2" }),
+                gsap.to("nav", { backgroundColor: "white" })
+        },
     },
     x: -horizontalScrollLength,
     ease: "none"
 });
-
-
-
-
-
-// ScrollTrigger.create(
-//     {
-//         trigger: "#about",
-//         start: "center center",
-//         pin: true,
-//         onEnter: () => {
-//             gsap.to(".intro-section", { backgroundColor: "black" }),
-//                 gsap.to("nav", { backgroundColor: "white" })
-//         },
-//         onLeaveBack: () => gsap.to(".intro-section", { backgroundColor: "white" }),
-//     }
-// )
